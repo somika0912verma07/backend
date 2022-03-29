03afb5dbe7a2ff30b693de4c99ae5add64ff5dd8
somika0912verma07 <somika.verma@gmail.com>
2022-03-29 16:15:40 +0530

    subham ne kiya

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,11 +3,11 @@ const User=require('../model/user.model');
 
 exports.signup=(request,response,next)=>{
     console.log(request.body)
-    const user=new User();
-    user.name=request.body.name;
-    user.email=request.body.email;
-    user.password=request.body.password;
-    user.save()
+    // const user=new User();
+    // user.name=request.body.name;
+    // user.email=request.body.email;
+    // user.password=request.body.password;
+    user.create(req.body)
     .then(result=>{
         return response.status(201).json(result);
     })
