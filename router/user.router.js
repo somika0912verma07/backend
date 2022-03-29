7ebe17521cf3e95ab10abbb7c05874eadf2cdae0
somika0912verma07 <somika.verma@gmail.com>
2022-03-29 18:22:11 +0530

    shubham change 2

--- router/user.router.js
+++ router/user.router.js
@@ -1,7 +1,12 @@
+const { request } = require('express');
 const express=require('express');
 const router=express.Router();
 const userController=require('../controller/user.controller');
 
+
+router.get('/',(request,response)=>{
+    response.send('working');
+})
 router.post('/signup',userController.signup);
 
 
